e2dbec02da0b28ea2710a5322951ec290bd60fe2
ryaanpng <85208154+ryaanpng@users.noreply.github.com>
2023-04-22 14:52:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
+app = Flask(__name__)
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+# url = ('https://elements.envato.com/pt-br/barbecue-monster-indie-rock-flyer-U2JELAV')
+
+@app.route('/pegar', methods=['POST'])
+def pegar():
+  url = request.json['url']
+  navegador = webdriver.Chrome(options=options)
+  navegador.get(url)
+  site = BeautifulSoup(navegador.page_source, 'html.parser')
+  titulo = site.find("meta", property='og:title').get('content')
+  imagem = site.find("meta", property='og:image').get('content')
+  return jsonify({
+              'titulo': titulo,
+              'imagem': imagem,
+  })
 app = Flask(__name__)
 
 
