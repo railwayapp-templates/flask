863c4ecf52f23e5da6b4a75b06fe4eda38eb7188
Joe <42589294+0xOsprey@users.noreply.github.com>
2024-05-09 22:21:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/api', methods=['POST'])
 def webhook():
     print(request.json)
     return '', 200
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'Hello World!', 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
